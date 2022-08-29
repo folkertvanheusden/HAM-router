1fa8769242ee41d1ec2d11bce0dca7c0d0832faa
Folkert van Heusden <mail@vanheusden.com>
2022-08-29 10:55:27 +0100

    restructuring utils.cpp

--- utils.h
+++ utils.h
@@ -1,16 +1,4 @@
 // (C) 2017-2022 by folkert van heusden, released under Apache License v2.0
 #pragma once
-#include <stdint.h>
-#include <string>
-#include <vector>
 
-uint64_t    get_us();
-
-std::string myformat(const char *const fmt, ...);
-std::string dump_hex(const unsigned char *p, int len);
-std::string dump_replace(const unsigned char *p, int len);
-std::vector<std::string> split(std::string in, std::string splitter);
-
-void       *duplicate(const void *in, const size_t size);
-
-uint32_t    calc_crc32(const uint8_t *const p, const size_t len);
+void *duplicate(const void *in, const size_t size);
